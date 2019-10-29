f1ea91752469b9cd505f5ed93ab08f4028406c5d
Soporte UTNBA <soporte@frba.utn.edu.ar>
2019-10-28 21:38:21 -0300

    Agregar archivos de proyecto.

---
+++ ToDoList/Model/DataStore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ToDoList.Model
+{
+    private ObservableCollection<Task> tasks;
+
+
+    public class DataStore
+    {
+        public DataStore()
+        {
+            tasks = new ObservableCollection<Task>();
+        }
+
+        public void Add (string nombre)
+        {
+            Task.Add(new Task { Name = nombre });
+        }
+    }
+}
---
+++ ToDoList/Model/Task.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoList.Model
+{
+    class Task //representa una tarea en particular
+    {
+
+        public string Name { get; set; }
+        public bool Completed { get; set; }
+    }
+}
